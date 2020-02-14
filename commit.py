6fc31d910d632c666f59b7f44e0c55e4adde5193
Gabriel Rodrigues Segalla <gabriel.segalla@seventh.com.br>
2020-02-14 15:52:27 -0300

    first commit

---
+++ commit.py
@@ -0,0 +1,43 @@
+import git
+import sys
+import subprocess
+
+repo = git.Repo('./')
+g = git.cmd.Git('./')
+
+print("Executando Formatação...")     
+subprocess.check_call('npm run format', shell=True)
+
+print("GIT ADD .")
+subprocess.check_call('git add .', shell=True)
+
+#add_all_files = input("Deseja Adicionar todos os arquivos no commit? (Y/n)")
+#if(add_all_files.upper() == 'Y'): 
+#    subprocess.check_call('git add .', shell=True)
+#elif(add_all_files.lower() == 'n'):
+#    
+#    files = []
+#    for file_id ,untracked_file in enumerate(repo.untracked_files, start=1):
+#        my_file = {}
+#        my_file['id'] = file_id
+#        my_file['file']= untracked_file
+#        files.append(my_file)
+#
+#    print("Choose the files to add in commit")
+#    for file in files:
+#        print('[{}] - {}'.format(file.get('id'),file.get('file')))
+#    selected_files = list(map(int, input("Enter a multiple value: ").split())) 
+#    for file in files:
+#        if file.get('id') in selected_files:
+#            g.add(file.get('file'))
+
+print("GIT COMMIT...") 
+if('commit.py' in sys.argv):
+    sys.argv = sys.argv[1: len(sys.argv)] 
+listToStr = ' '.join([str(elem) for elem in sys.argv]) 
+message = '[{}] - {}'.format(repo.active_branch.name, listToStr)
+index = repo.index
+index.commit(message)
+
+#
+#g.pull()
